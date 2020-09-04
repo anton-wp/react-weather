f0392c9c78986cb8d325dc20bc836c8ab9765fa6
user <anton69917091@gmail.com>
2020-09-04 12:49:43 +0300

    ##

--- src/components/AddCityBlock.js
+++ src/components/AddCityBlock.js
@@ -22,7 +22,6 @@ class AddCityBlock extends React.Component {
 
       let newCity = [
         ...this.props.weather.id,
-        
         ...[this.state.selectedOption.id],
       ]
       localStorage.setItem('city', newCity)
